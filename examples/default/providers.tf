edf5b41f578ff9f1ee3922fd4c815aa8fdca7cae
ravi <ravi.periketi@fxinnovation.com>
2020-02-11 12:56:19 -0500

    update ad provider block

--- examples/default/providers.tf
+++ examples/default/providers.tf
@@ -8,6 +8,8 @@ provider "azurerm" {
 
 provider "azuread" {
   version         = "~>0.3.0"
+  client_id       = var.client_id
+  client_secret   = var.client_secret
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
 }
